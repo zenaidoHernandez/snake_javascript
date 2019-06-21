8d53eba9aca7820daf3c2a019226bb9d01a862f3
DESKTOP-J3HJ93Q\ZHGONZALEZ <goz.694@gmail.com>
2019-06-21 18:09:52 -0500

    create and eat food

--- snake.js
+++ snake.js
@@ -8,6 +8,8 @@ const head = {
     y: 0
 };
 
+let food = null;
+
 let dx = 0;
 let dy = 0;
 
@@ -20,15 +22,34 @@ function main(){
 function update(){
     head.x += dx;
     head.y += dy;
+
+    if(food && head.x === food.x && head.y === food.y){
+        food = null;
+    }
+
+    if(!food){
+        food = {x: getRandomX(), y: getRandomY()}
+    }
+}
+
+function getRandomX(){
+    return SIZE * (parseInt(Math.random() * 20));
+}
+
+function getRandomY(){
+    return SIZE * (parseInt(Math.random() * 23));
 }
 
 function draw(){
-    context.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    drawHead(head);
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, myCanvas.width, myCanvas.height);
+    drawObject(head, 'lime');
+    drawObject(food, 'white');
 };
 
-function drawHead(head){
-    context.fillRect(head.x, head.y, SIZE, SIZE);
+function drawObject(obj, color){
+    context.fillStyle = color;
+    context.fillRect(obj.x, obj.y, SIZE, SIZE);
 }
 
 document.addEventListener('keydown', moveSnake);
